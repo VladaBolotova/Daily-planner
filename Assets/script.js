0249cd6233fb096ac89f0cbeaf3446fb129307ad
Vlada <111080921+VladaBolotova@users.noreply.github.com>
2022-10-31 22:42:02 -0400

    scriptjs

---
+++ Assets/script.js
@@ -0,0 +1,36 @@
+const store = window.localStorage;
+
+const container = $(".container");
+
+const now = moment();
+
+const Time = { text: moment().format("h:00 A"), hour: moment().hour() };
+
+$("#day").text(now.format("dddd MMMM DD[th] "));
+
+var currentTime=moment().hour();
+var allTimeBlocks = document.getElementsByClassName('hour')
+
+for (var i=0;i<allTimeBlocks.length; i++) {
+//convir a string into a numbere//
+  if (parseInt(allTimeBlocks[i].id) < currentTime) {
+    var textArea = allTimeBlocks[i].nextElementSibling
+    textArea.classList.add( 'past')
+  }
+  else if (parseInt(allTimeBlocks[i].id) == currentTime) {
+    var textArea = allTimeBlocks[i].nextElementSibling
+    textArea.classList.add( 'present')
+  }
+  else {
+    var textArea = allTimeBlocks[i].nextElementSibling
+    textArea.classList.add( 'future')
+  }
+};
+
+var addButton=document.querySelector("button");
+addButton.addEventListener("click", function() {
+  var textContent=this.previousElementSibling.value;
+  console.log(textContent);
+  localStorage.setItem("textcontent", textContent);
+});
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Daily-planner
+# 05 Third-Party APIs: Work Day Scheduler
+
+Description:
+
+This is a simple calendar application that allows a user to save events for each hour of the day. This app runs in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+Usage:
+
+The page presents a daily planner to create a schedule. When user opens a planner the current day is displayed at the top of the calendar, by scrolling down the page presented with timeblocks for standart bussiness hours. The users can see that each timeblock is color coded to indicate whether it is in the past, presnt or future. By clicking into a timeblock you can enter an event and save it with a save buttin. 
+
+Credits N/A
+
+
